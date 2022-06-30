68fbb15e8391331d77e0a5213efd168f298f1f73
kojeffi <omondijeff88@gmail.com>
2022-06-30 12:04:25 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - return the integal 0 from the main function
+ *Description:"with proper grammar, but the outcome is a piece of art,"
+* Return: o from the main function
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
