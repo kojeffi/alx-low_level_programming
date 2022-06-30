345d0f45afb280194d2d7e5310ffee7d0cfaad45
kojeffi <omondijeff88@gmail.com>
2022-06-30 12:17:44 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - use to return the integer value 1
+ *Description: use to print to standard output in the main function
+      *Return: return the value 1
+ */
+int main(void)
+
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
+}
