799ad6ef226f5319db0cc4f2f0a86e219a8d8b98
kojeffi <omondijeff88@gmail.com>
2022-07-06 05:48:05 -0700

    Tasks 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+{
+putchar(i);
+}
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
