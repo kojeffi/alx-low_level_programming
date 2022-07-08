28d8b164a54df38ae75a44392db7d0857846c886
kojeffi <omondijeff88@gmail.com>
2022-07-07 17:47:59 -0700

    a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 #include <stdio.h>
 /**
@@ -7,12 +6,10 @@
 * and for the multiples of five prints Buzz
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
-
 {
 
 if (i % 3 == 0 && i % 5 != 0)
