76f9df0a51c0080addb149b1b799dbe730fa956a
kojeffi <omondijeff88@gmail.com>
2022-07-07 18:15:10 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,30 +11,27 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-
 if (i % 3 == 0 && i % 5 != 0)
 {
-
 printf(" Fizz");
-} else if (i % 5 == 0 && i % 3 != 0)
+}
+else if (i % 5 == 0 && i % 3 != 0)
 {
-
 printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+else if (i % 3 == 0 && i % 5 == 0)
 {
-
 printf(" FizzBuzz");
-} else if (i == 1)
+}
+else if (i == 1)
 {
-
 printf("%d", i);
-} else
+}
+else
 {
-
 printf(" %d", i);
 }
 }
 printf("\n");
-
 return (0);
 }
